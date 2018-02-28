473368cb87f0bd7e98e8c949c212fb4f4f724802
Alberto Sartori <alberto.sartori86@gmail.com>
2018-02-28 14:06:10 +0100

    working spring

--- examples/streamer/streamer_interface.h
+++ examples/streamer/streamer_interface.h
@@ -16,6 +16,15 @@ public:
 
   // interface with the PDESystemInterface :)
 
+void declare_parameters(ParameterHandler& prm){
+
+  PDESystemInterface<dim,spacedim,StreamerModel<dim,spacedim,LAC>, LAC >::declare_parameters(prm);
+  this->add_parameter(prm, &rho, "rho linear", "0.0", Patterns::Double(0.0));
+  this->add_parameter(prm, &k, "elasticity constant", "1.0", Patterns::Double(0.0));
+  this->add_parameter(prm, &eta, "viscosity", "1.0", Patterns::Double(0.0));
+
+
+}
 
   template <typename EnergyType, typename ResidualType>
   void energies_and_residuals(const typename DoFHandler<dim,spacedim>::active_cell_iterator &cell,
@@ -24,6 +33,10 @@ public:
                               std::vector<std::vector<ResidualType> > &local_residuals,
                               bool compute_only_system_terms) const;
 
+ private:
+double rho;
+double k;
+double eta;
 };
 
 template <int dim, int spacedim, typename LAC>
@@ -49,17 +62,23 @@ energies_and_residuals(const typename DoFHandler<dim,spacedim>::active_cell_iter
 {
 
   const FEValuesExtractors::Vector displ(0);
+  const FEValuesExtractors::Vector vel(spacedim);
 
   ResidualType rt = 0; // dummy number to define the type of variables
   this->reinit (rt, cell, fe_cache);
   const double alpha = this->get_alpha();
-  fe_cache.cache_local_solution_vector("previous_explicit_solution", this->get_locally_relevant_previous_explicit_solution(),alpha);
+  // fe_cache.cache_local_solution_vector("previous_explicit_solution", this->get_locally_relevant_previous_explicit_solution(),alpha);
   auto &uts = fe_cache.get_values("solution_dot", "u", displ, rt);
   auto &graduts = fe_cache.get_gradients("solution_dot", "du", displ, rt);
   auto &gradus = fe_cache.get_gradients("solution", "du", displ, rt);
-  /* auto &us = fe_cache.get_values("solution", "u", displ, rt); */
-  /* auto &us_2 = fe_cache.get_values("previous_explicit_solution", "u", displ, alpha); */
-  /* auto &us_1 = fe_cache.get_values("explicit_solution", "u", displ, alpha); */
+
+  // auto &gradvs = fe_cache.get_gradients("solution", "dv", vel, rt);
+  // auto &vs     = fe_cache.get_values("solution", "v", vel, rt);
+  // auto &vts    = fe_cache.get_values("solution_dot", "v_dot", vel, rt);
+  
+  // auto &us = fe_cache.get_values("solution", "u", displ, rt);
+  // auto &us_2 = fe_cache.get_values("previous_explicit_solution", "u", displ, alpha);
+  // auto &us_1 = fe_cache.get_values("explicit_solution", "u", displ, alpha);
 
   const unsigned int n_q_points = uts.size();
   auto &JxW = fe_cache.get_JxW_values();
@@ -73,26 +92,30 @@ energies_and_residuals(const typename DoFHandler<dim,spacedim>::active_cell_iter
 //  vv[2] = 0;
   for (unsigned int q=0; q<n_q_points; ++q)
     {
-    auto &ut = uts[q];
-    /* auto &u_2 = us_2[q]; */
-    /* auto &u_1 = us_1[q]; */
-      /* auto &u = us[q]; */
+      auto &ut = uts[q];
+      // auto &v = vs[q];
+      // auto &vt = vts[q];
       auto &gradu = gradus[q];
+      // auto &gradv = gradvs[q];
       for (unsigned int i=0; i<local_residuals[0].size(); ++i)
         {
-	  const double k = 1.;
-          auto v = fev[displ].value(i,q);
-          auto gradv = fev[displ].gradient(i,q);
+	  // const double k = 1.;
+          auto phi_u = fev[displ].value(i,q);
+          auto grad_phi_u = fev[displ].gradient(i,q);
+          // auto phi_v = fev[vel].value(i,q);
+          // auto grad_phi_v = fev[vel].gradient(i,q);
           local_residuals[0][i] += (
-                                  /* (u*v)*alpha*alpha*1000. -1000.*2.*alpha*alpha*(u_1*v) + (u_2*v)*alpha*alpha*1000. */
-                                     /* + */
-                                     k*scalar_product(gradu,gradv)
-//                + 10.*(ut*v)
-                + scalar_product(graduts[q],gradv)
-                +ut*v
-//                                    + 0.9*(ut[0]-vv)
-//                                   -vv
-                                   )*JxW[q];
+				    // rho*vt*phi_u +
+				    // 1e5*(v-ut)*phi_v + 
+				    // (u*phi_u)*alpha*alpha*rho -rho*2.*alpha*alpha*(u_1*phi_u) + (u_2*phi_u)*alpha*alpha*rho
+				    // +
+				    k*scalar_product(gradu,grad_phi_u)
+
+				    + eta*scalar_product(graduts[q],grad_phi_u)
+				    
+				    +ut*phi_u
+
+				    )*JxW[q];
         }
 
       (void)compute_only_system_terms;
